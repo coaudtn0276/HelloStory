360302211c5e2a49c5f097fe515a8cf093280761
coaudtn0276 <coaudtn0276@gmail.com>
2024-01-27 19:47:07 +0900

    createObjectURL사용 이미지추가

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { Dropdown } from "@/components";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Write = () => {
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileName, setFileName] = useState("");
+
   const dropDownList = ["게임", "맛집", "반려동물", "잡담"];
   const contentEditableRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
     if (contentEditableRef.current !== null) {
       console.log(contentEditableRef.current.innerHTML);
+    }
+  };
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      const file = e.target.files[0];
+      setPreviewImage(URL.createObjectURL(file));
+      setFileName(file.name);
+      // console.log(file.name);
     }
   };
 
@@ -23,11 +35,30 @@ const Write = () => {
         <p>제목</p>
         <input type="text" className="w-full border-2 border-gray-primary rounded-lg h-8 text-[8px] sm:text-[8px] md:text-[10px] lg:text-xs pl-2" placeholder="제목을 입력해 주세요." />
       </div>
-      <div>
+      <div className="mb-2">
         <p>내용</p>
-        <div ref={contentEditableRef} contentEditable="true" className="text-xs"></div>
-        {/* <p>사진을 넣으면 사진 넣을곳</p> */}
+        <div ref={contentEditableRef} contentEditable="true" className="text-xs w-full h-96 border-2 rounded-lg p-2 border-gray-primary">
+          {previewImage && (
+            <p className="w-36">
+              <img src={previewImage} alt="previewImage" className="object-cover" />
+            </p>
+          )}
+        </div>
+
         <button onClick={handleClick}>버튼</button>
+      </div>
+      <div>
+        <p>첨부파일</p>
+        <div className="flex justify-between">
+          <p className="flex-auto border-2 border-gray-primary rounded-lg px-2">{fileName ? fileName : "최대 5MB, 확장자 jpg, png, gif"}</p>
+
+          <div>
+            <label htmlFor="file-upload" className="w-full h-8 border-2 border-gray-primary rounded-lg cursor-pointer">
+              파일 찾기
+            </label>
+            <input id="file-upload" type="file" accept="image/*" className="hidden" onChange={handleImageUpload} />
+          </div>
+        </div>
       </div>
     </div>
   );
